d3918417c13acba31e6eae7e9bb81e9404aff237
Adit Mukherjee <mukher23@purdue.edu>
2019-04-30 02:39:41 -0400

    Add a bunch of FMOD header files and my edits

--- tictactoe.c
+++ tictactoe.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <fmod.h>
+#include <fmod_errors.h>
 
 //3x3
 struct BasicBoard
@@ -24,6 +26,24 @@ struct UltimateMove
 };
 
 int main() {
+    //FMOD stuff
+    FMOD_RESULT result;
+    FMOD::Studio::System* system = NULL;
+
+    result = FMOD::Studio::System::create(&system); // Create the Studio System object.
+    if (result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        exit(-1);
+    }
+
+    // Initialize FMOD Studio, which will also initialize FMOD Low Level
+    result = system->initialize(512, FMOD_STUDIO_INIT_NORMAL, FMOD_INIT_NORMAL, 0);
+    if (result != FMOD_OK)
+    {
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        exit(-1);
+    }
 	//function prototypes
 	struct BasicBoard *createBasicTicTacToeBoard();
 	struct UltimateBoard *createUltimateTicTacToeBoard();
